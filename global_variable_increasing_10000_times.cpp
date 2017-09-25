24865903182fc5d6398b9a6a2ed203dfb1a18996
amoo-milad <30766645+amoo-milad@users.noreply.github.com>
2017-09-25 23:02:29 +0330

    new

--- global_variable_increasing_10000_times.cpp
+++ global_variable_increasing_10000_times.cpp
@@ -6,35 +6,34 @@
 
 int data = 0;
 int increment = 1;
-HANDLE* func();
+void func();
 DWORD WINAPI MyThreadFunc(LPVOID lpParameter);
 
 int main()
 {
-	HANDLE handles = func();
+	func();
 	
-	WaitForMultipleObjects(4,&handles,TRUE,INFINITE); // is '&handles' right?
 	printf("\n* Global Data is : %d *\n\n", data);
 	
 	_getch();
 	return 0;           
 }
 
-HANDLE* func()
+void func()
 {
 	HANDLE h1 = CreateThread(NULL, 0, &MyThreadFunc, 0, 0, NULL);
 	HANDLE h2 = CreateThread(NULL, 0, &MyThreadFunc, 0, 0, NULL);
 	HANDLE h3 = CreateThread(NULL, 0, &MyThreadFunc, 0, 0, NULL);
 	HANDLE h4 = CreateThread(NULL, 0, &MyThreadFunc, 0, 0, NULL);
 	HANDLE handles[] = {h1,h2,h3,h4};
-	return handles;
+	WaitForMultipleObjects(4,handles,TRUE,INFINITE); // is '&handles' right?
 }
 
 DWORD WINAPI MyThreadFunc(LPVOID lpParameter)
 {
-	for (int i = 0; i < 10000; i++)
-		data++;
-	//	data = data + increment;
+	for (int i = 0; i < 10000000; i++)
+		data = data + increment;
+		//++data;
 
 	printf("now data is: %d\t end of thread!\n\n", data);
 	return 0;
